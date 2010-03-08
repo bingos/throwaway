dc11761bd7d11d8bbf57328cc1eb74c19fb86c80
Chris Williams <chris@bingosnet.co.uk>
2010-03-08 10:29:44 +0000

    Added an --mi switch to the check_installed script to generate requires lists for Module::Install based Makefile.PL

--- check_installed.pl
+++ check_installed.pl
@@ -1,9 +1,19 @@
 use strict;
 use warnings;
+use Getopt::Long;
 use Module::Load::Conditional qw(check_install);
+
+my $mi;
+
+GetOptions( 'mi' => \$mi );
 
 foreach my $module ( sort @ARGV ) {
   my $href = check_install( module => $module );
   next unless $href;
-  print join(',',$module,(defined $href->{version} ? $href->{version} : '')), "\n";
+  if ( $mi ) {
+    print join(' ', 'requires', qq{'$module'}, '=>', (defined $href->{version} ? qq{'$href->{version}'} : q{'0'}) ), "\n";
+  } 
+  else {
+    print join(',',$module,(defined $href->{version} ? $href->{version} : '')), "\n";
+  }
 }
